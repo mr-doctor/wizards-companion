08d065f407fc6132236e9b1982b1f156b922ab43
daniel <daniel@gilbertpinfold.co.nz>
2018-08-09 16:09:02 +1200

    Add correct Firebase support

--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,11 +1,8 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Firebase} from '@ionic-native/firebase';
 import {SpellModel} from "../page/page";
-import {AngularFireStorage} from "angularfire2/storage";
-import {AngularFireDatabase} from "angularfire2/database";
-import * as firebase2 from 'firebase';
 import {environment} from "@app/env";
+import firebase from "firebase";
 
 /*
   Generated class for the FirebaseProvider provider.
@@ -19,26 +16,14 @@ export class FirebaseProvider {
   public token: string;
   private userProfileCollection: any;
   
-  constructor(private firebase: Firebase, private db: AngularFireDatabase, private afStorage: AngularFireStorage) {
-    this.firebase.getToken()
-      .then(token => this.token = token) // save the token server-side and use it to push notifications to this device
-      .catch(error => console.error('Error getting token', error));
-    
-    this.firebase.onTokenRefresh()
-      .subscribe((token: string) => this.token = token);
-    firebase2.initializeApp(environment.firebase);
-    
-    this.userProfileCollection = firebase2.firestore.collection<any>('userProfile');
-  
-   this.userProfileCollection.push({
-      name: 'Jorge Vergara',
-      email: 'j@javebratt.com',
-      // Other info you want to add here
-   });
+  constructor() {
+    firebase.initializeApp(environment.firebase);
+
   }
   
   uploadSpell(spell: SpellModel) {
-  
+    var spells = firebase.firestore().collection("test spellbook");
+    console.log(spells.get());
   }
   
   
