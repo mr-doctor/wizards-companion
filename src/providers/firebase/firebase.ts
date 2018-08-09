bc4c60476e6e657019315429c40f44bc8663baa0
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2018-08-10 07:50:05 +1200

    Add proper Firebase reading

--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,8 +1,9 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {SpellModel} from "../page/page";
-import {environment} from "@app/env";
-import firebase from "firebase";
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import QuerySnapshot = firebase.firestore.QuerySnapshot;
 
 /*
   Generated class for the FirebaseProvider provider.
@@ -14,16 +15,38 @@ import firebase from "firebase";
 export class FirebaseProvider {
   
   public token: string;
-  private userProfileCollection: any;
   
   constructor() {
-    firebase.initializeApp(environment.firebase);
-
+    var config = {
+      apiKey: "AIzaSyB3WtVe2EjpaNhcvey944ywVx3JjMNtTNU",
+      authDomain: "wizard-s-companion.firebaseapp.com",
+      databaseURL: "https://wizard-s-companion.firebaseio.com",
+      projectId: "wizard-s-companion",
+      storageBucket: "wizard-s-companion.appspot.com",
+      messagingSenderId: "112408276315"
+    };
+    firebase.initializeApp(config);
+    
   }
   
   uploadSpell(spell: SpellModel) {
-    var spells = firebase.firestore().collection("test spellbook");
-    console.log(spells.get());
+    var collectionReference = firebase.firestore().collection("Spells");
+    
+    collectionReference.get().then( querySnapshot => {
+      console.log("Found collection");
+      this.displaySpells(querySnapshot)
+    });
+  }
+  
+  displaySpells(querySnapshot: QuerySnapshot) {
+    if (querySnapshot.empty) {
+      console.log("No docs found");
+      return;
+    }
+    querySnapshot.forEach(function (documentSnapshot) {
+      var data = documentSnapshot.data();
+      console.log(data);
+    });
   }
   
   
